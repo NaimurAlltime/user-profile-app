f6f82eba7c4c9ceac6b02d66df1af8d705831cb7
Naimur Rahman <naimur2231@gmail.com>
2023-09-20 22:59:54 +0600

    basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="flex justify-center items-center">
+          <img src="/user-profile-logo.png" alt="user profile" />
+          <h2 className="text-3xl font-semibold text-center ml-1">
+            User Profile App
+          </h2>
+        </div>
+        <div className="border-t-2 border-gray-600 mx-auto w-72"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
